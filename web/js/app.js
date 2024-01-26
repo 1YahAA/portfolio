dab2f3aefdf9e9c69b5b0bf03a96a04508b4e06d
1YahAA <120428519+1YahAA@users.noreply.github.com>
2024-01-26 17:33:14 +1000

    Delete web directory

--- web/js/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
-})
-
-function scrollToProjects() {
-    var projectsSection = document.getElementById('projectsSection');
-    projectsSection.scrollIntoView({ behavior: 'smooth' });
-}
-
